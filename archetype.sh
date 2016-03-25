bab80b57d258843e1338dcd421719ad35cbdb8cf
slavb18 <slavb18@gmail.com>
2016-03-25 09:40:40 +0400

    common version, archetype fix

--- archetype.sh
+++ archetype.sh
@@ -4,6 +4,6 @@ rm -rf jparestresource-archetype
 mvn clean archetype:create-from-project -Darchetype.properties=archetype.properties
 mv target/generated-sources/archetype jparestresource-archetype
 grep -Rl jparestresource jparestresource-archetype/src/main/resources/archetype-resources|xargs sed -i 's/jparestresource/${rootArtifactId}/g'
-mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/resources/ru/ilb/jparestresource jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/resources/ru/ilb/__rootArtifactId__
+mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/jparestresource jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
 cd jparestresource-archetype
 mvn clean install
