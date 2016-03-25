cb084046edd3e736c47cf2a869e15b63846513a6
slavb18 <slavb18@gmail.com>
2016-03-25 09:56:35 +0400

    archetype fix

--- archetype.sh
+++ archetype.sh
@@ -5,5 +5,6 @@ mvn clean archetype:create-from-project -Darchetype.properties=archetype.propert
 mv target/generated-sources/archetype jparestresource-archetype
 grep -Rl jparestresource jparestresource-archetype/src/main/resources/archetype-resources|xargs sed -i 's/jparestresource/${rootArtifactId}/g'
 mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/jparestresource jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
+mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/document jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
 cd jparestresource-archetype
 mvn clean install
--- jparestresource-archetype/src/main/resources/archetype-resources/archetype.sh
+++ jparestresource-archetype/src/main/resources/archetype-resources/archetype.sh
@@ -7,6 +7,7 @@ rm -rf ${artifactId}-archetype
 mvn clean archetype:create-from-project -Darchetype.properties=archetype.properties
 mv target/generated-sources/archetype ${artifactId}-archetype
 grep -Rl ${artifactId} ${artifactId}-archetype/src/main/resources/archetype-resources|xargs sed -i 's/${artifactId}/${symbol_dollar}{rootArtifactId}/g'
-mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/resources/${packageInPathFormat} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/resources/ru/ilb/__rootArtifactId__
+mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/${packageInPathFormat} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
+mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/${object_urn} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
 cd ${artifactId}-archetype
 mvn clean install
