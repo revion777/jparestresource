c6ffa57386848607142408bc076beee76e01b3bc
slavb18 <slavb18@gmail.com>
2016-04-12 09:19:22 +0400

    archetype update

--- archetype/archetype.sh
+++ archetype/archetype.sh
@@ -7,5 +7,6 @@ mv target/generated-sources/archetype jparestresource-archetype
 grep -Rl jparestresource jparestresource-archetype/src/main/resources/archetype-resources|xargs sed -i 's/jparestresource/${rootArtifactId}/g'
 mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/jparestresource jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
 mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/document jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
+sed -i 's/document.xjb/${object_urn}.xjb/' jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/pom.xml
 cd jparestresource-archetype
 mvn clean install
--- jparestresource-archetype/src/main/resources/archetype-resources/archetype/archetype.sh
+++ jparestresource-archetype/src/main/resources/archetype-resources/archetype/archetype.sh
@@ -10,5 +10,6 @@ mv target/generated-sources/archetype ${artifactId}-archetype
 grep -Rl ${artifactId} ${artifactId}-archetype/src/main/resources/archetype-resources|xargs sed -i 's/${artifactId}/${symbol_dollar}{rootArtifactId}/g'
 mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/${packageInPathFormat} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
 mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/${object_urn} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
+sed -i 's/${object_urn}.xjb/${symbol_dollar}{object_urn}.xjb/' ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/pom.xml
 cd ${artifactId}-archetype
 mvn clean install
