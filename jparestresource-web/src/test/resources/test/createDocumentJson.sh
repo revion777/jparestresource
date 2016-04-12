cd9bd5520acbd6a7326ee656fc57ff618c7d1ba2
slavb18 <slavb18@gmail.com>
2016-04-12 10:22:24 +0400

    new layout for ExceptionHandlers

--- jparestresource-web/src/test/resources/test/createDocumentJson.sh
+++ jparestresource-web/src/test/resources/test/createDocumentJson.sh
@@ -1,1 +1,1 @@
-curl -v -X POST -H "Content-Type: application/json" -u ide:123 -d @createDocumentJson.json -o createDocumentJson.txt http://localhost:8080/jparestresource-ws/web/documents
+curl -v -X POST -H "Content-Type: application/json" -u ide:123 -d @createDocumentJson.json -o createDocumentJson.txt http://localhost:8080/jparestresource-web/web/documents
