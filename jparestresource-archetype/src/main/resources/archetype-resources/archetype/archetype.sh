be263ffef53baeb71b47cfb7a1ab537d0f83dcb2
slavb18 <slavb18@gmail.com>
2016-04-27 12:05:11 +0400

    using META-INF/services/javax.xml.bind.JAXBContext istead of jaxb.properties

--- jparestresource-archetype/src/main/resources/archetype-resources/archetype/archetype.sh
+++ jparestresource-archetype/src/main/resources/archetype-resources/archetype/archetype.sh
@@ -8,8 +8,8 @@ rm -rf ${artifactId}-archetype
 mvn clean archetype:create-from-project -Darchetype.properties=archetype/archetype.properties
 mv target/generated-sources/archetype ${artifactId}-archetype
 grep -Rl ${artifactId} ${artifactId}-archetype/src/main/resources/archetype-resources|xargs sed -i 's/${artifactId}/${symbol_dollar}{rootArtifactId}/g'
-mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/${packageInPathFormat} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
-mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/${object_urn} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
+${symbol_pound}mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/${packageInPathFormat} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
+${symbol_pound}mv ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/${object_urn} ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
 sed -i 's/${object_urn}.xjb/${symbol_dollar}{object_urn}.xjb/' ${artifactId}-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/pom.xml
 patch -p0 < archetype/pom.xml.diff
 cd ${artifactId}-archetype
