b0d159c32e870be38750171efbfa3c195819e343
slavb18 <slavb18@gmail.com>
2016-07-09 00:12:09 +0400

    searchcondition filter

--- jparestresource-web/src/main/java/ru/ilb/jparestresource/web/DocumentResourceImpl.java
+++ jparestresource-web/src/main/java/ru/ilb/jparestresource/web/DocumentResourceImpl.java
@@ -12,16 +12,23 @@ import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import org.apache.cxf.jaxrs.ext.search.SearchCondition;
+import org.apache.cxf.jaxrs.ext.search.SearchContext;
+import org.apache.cxf.jaxrs.ext.search.SearchParseException;
 import org.apache.cxf.jaxrs.provider.JAXBElementProvider;
 import org.eclipse.persistence.config.QueryHints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeanUtils;
 import org.springframework.transaction.annotation.Transactional;
+import ru.ilb.common.jaxrs.search.JPAOrderedQueryVisitor;
 import ru.ilb.jparestresource.jparestresource.Document;
 import ru.ilb.jparestresource.jparestresource.Documents;
 import ru.ilb.jparestresource.api.DocumentsResource;
@@ -36,21 +43,51 @@ public class DocumentResourceImpl implements DocumentsResource {
     @Resource(name="jaxbProvider")
     JAXBElementProvider jaxbElementProvider;
 
-    @Autowired
-    EntityManagerIntr entityManagerIntr;
+//    @Autowired
+//    EntityManagerIntr entityManagerIntr;
+    
+    private UriInfo uriInfo;
+    @Context
+    public void setUriInfo(UriInfo uriInfo) {
+        this.uriInfo = uriInfo;
+    }
+    
+    private SearchContext searchContext;
+
+    @Context
+    public void setSearchContext(SearchContext searchContext) {
+        this.searchContext = searchContext;
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(DocumentResourceImpl.class);
 
     @Override
     @Transactional
-    public Documents list(List<ReadOptionsType> options) {
+    public Documents list(List<ReadOptionsType> options, String filter, Integer limit, String order) {
         Documents res = new Documents();
-
-        TypedQuery<Document> query = em.createQuery("SELECT d FROM Document d", Document.class);
+        JPAOrderedQueryVisitor<Document> visitor = new JPAOrderedQueryVisitor<>(em, Document.class);
+        TypedQuery<Document> query;
+        if(filter!=null ){
+            SearchCondition<Document> sc;
+            try {
+                sc = searchContext.getCondition(filter, Document.class);
+            } catch (SearchParseException ex){
+                throw new BadRequestException(ex);
+            }
+            sc.accept(visitor);
+            CriteriaQuery<Document> cq= order!=null ? visitor.getOrderedCriteriaQuery(visitor, order): visitor.getCriteriaQuery();
+            query=em.createQuery(cq);
+            
+        } else {
+            query = em.createQuery("SELECT d FROM Document d", Document.class);
+        }
         if (options != null) {
             if (options.contains(ReadOptionsType.WITH_DOCFILES)) {
                 query.setHint(QueryHints.LEFT_FETCH, "d.docfiles");
             }
+        }
+        if(limit!=null){
+            query.setMaxResults(limit);
         }
         List<Document> result = query.getResultList();
         //res.getDocuments().addAll(entityManagerIntr.copy(result, null, null));
@@ -78,7 +115,7 @@ public class DocumentResourceImpl implements DocumentsResource {
     @Transactional
     public void edit(UUID uid, Document document) {
         Document doc = find(uid);
-        document.copyTo(doc);
+        BeanUtils.copyProperties(document, doc,new String[] {"id"});
     }
 
     @Override
