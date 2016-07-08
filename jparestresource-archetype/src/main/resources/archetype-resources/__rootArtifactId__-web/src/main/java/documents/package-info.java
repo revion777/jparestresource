5432a9ed9b7f18cdaeccde4a432d782ba87eeb75
slavb18 <slavb18@gmail.com>
2016-07-09 00:54:02 +0400

    archetype update

--- jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/documents/package-info.java
+++ jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/documents/package-info.java
@@ -1,9 +1,9 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:ru:ilb:${parentArtifactId}:${parentArtifactId}", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:ru:ilb:${parentArtifactId}:documents", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 @XmlAccessorFactory(LazyAccessorFactoryImpl.class)
-package ${package}.${parentArtifactId};
+package ${package}.documents;
 
 import com.sun.xml.internal.bind.XmlAccessorFactory;
 import ru.ilb.common.jpa.jaxb.LazyAccessorFactoryImpl;
--- jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/__object_class__ResourceImpl.java
+++ jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/__object_class__ResourceImpl.java
@@ -15,20 +15,29 @@ import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import org.apache.cxf.jaxrs.ext.search.SearchCondition;
+import org.apache.cxf.jaxrs.ext.search.SearchContext;
+import org.apache.cxf.jaxrs.ext.search.SearchParseException;
 import org.apache.cxf.jaxrs.provider.JAXBElementProvider;
 import org.eclipse.persistence.config.QueryHints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import ${package}.${parentArtifactId}.${object_class};
-import ${package}.${parentArtifactId}.${object_class}s;
+import ru.ilb.common.jaxrs.jaxb.JaxbContextResolver;
+import ru.ilb.common.jaxrs.search.JPAOrderedQueryVisitor;
+import ${package}.documents.${object_class};
+import ${package}.documents.${object_class}s;
 import ${package}.api.${object_class}sResource;
-import ${package}.${parentArtifactId}.ReadOptionsType;
+import ${package}.documents.ReadOptionsType;
 
 @Path("documents")
 public class ${object_class}ResourceImpl implements ${object_class}sResource {
@@ -36,24 +45,50 @@ public class ${object_class}ResourceImpl implements ${object_class}sResource {
     @PersistenceContext(unitName = "${parentArtifactId}")
     private EntityManager em;
 
-    @Resource(name="jaxbProvider")
-    JAXBElementProvider jaxbElementProvider;
+    @Autowired JaxbContextResolver jaxbContextResolver;
+    
+    private UriInfo uriInfo;
+    @Context
+    public void setUriInfo(UriInfo uriInfo) {
+        this.uriInfo = uriInfo;
+    }
+    
+    private SearchContext searchContext;
 
-    @Autowired
-    EntityManagerIntr entityManagerIntr;
+    @Context
+    public void setSearchContext(SearchContext searchContext) {
+        this.searchContext = searchContext;
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(${object_class}ResourceImpl.class);
 
     @Override
     @Transactional
-    public ${object_class}s list(List<ReadOptionsType> options) {
+    public ${object_class}s list(List<ReadOptionsType> options, String filter, Integer limit, String order) {
         ${object_class}s res = new ${object_class}s();
-
-        TypedQuery<${object_class}> query = em.createQuery("SELECT d FROM ${object_class} d", ${object_class}.class);
+        JPAOrderedQueryVisitor<${object_class}> visitor = new JPAOrderedQueryVisitor<>(em, ${object_class}.class);
+        TypedQuery<${object_class}> query;
+        if(filter!=null ){
+            SearchCondition<${object_class}> sc;
+            try {
+                sc = searchContext.getCondition(filter, ${object_class}.class);
+            } catch (SearchParseException ex){
+                throw new BadRequestException(ex);
+            }
+            sc.accept(visitor);
+            CriteriaQuery<${object_class}> cq= order!=null ? visitor.getOrderedCriteriaQuery(visitor, order): visitor.getCriteriaQuery();
+            query=em.createQuery(cq);
+            
+        } else {
+            query = em.createQuery("SELECT d FROM ${object_class} d", ${object_class}.class);
+        }
         if (options != null) {
             if (options.contains(ReadOptionsType.WITH_DOCFILES)) {
                 query.setHint(QueryHints.LEFT_FETCH, "d.docfiles");
             }
+        }
+        if(limit!=null){
+            query.setMaxResults(limit);
         }
         List<${object_class}> result = query.getResultList();
         //res.get${object_class}s().addAll(entityManagerIntr.copy(result, null, null));
@@ -81,7 +116,7 @@ public class ${object_class}ResourceImpl implements ${object_class}sResource {
     @Transactional
     public void edit(UUID uid, ${object_class} document) {
         ${object_class} doc = find(uid);
-        document.copyTo(doc);
+        BeanUtils.copyProperties(document, doc,new String[] {"id"});
     }
 
     @Override
@@ -95,7 +130,7 @@ public class ${object_class}ResourceImpl implements ${object_class}sResource {
     @Transactional
     public void init() {
         try {
-            JAXBContext jaxbContext=jaxbElementProvider.getJAXBContext(${object_class}s.class, null);
+            JAXBContext jaxbContext = jaxbContextResolver.getContext(${object_class}s.class);
             ${object_class}s documents=(${object_class}s) jaxbContext.createUnmarshaller().unmarshal(${object_class}s.class.getResourceAsStream("/META-INF/xml/testdata.xml"));
             for(${object_class} doc:documents.get${object_class}s()){
                 create(doc);
