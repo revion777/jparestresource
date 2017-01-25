a4598fd1e40bc1196f4d6aa760aeb37414b844eb
slavb18 <slavb18@gmail.com>
2017-01-25 08:48:23 +0400

    api-docs

---
+++ jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/repositories/__domain_objectName__Repository.java
@@ -0,0 +1,35 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Copyright 2016 slavb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ${groupId}.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import ${groupId}.${domain_packageName}.${domain_objectName};
+
+/**
+ *
+ * @author slavb
+ */
+@Transactional
+public interface ${domain_objectName}Repository extends JpaRepository<${domain_objectName},Long> { 
+    
+    public ${domain_objectName} findByUid(UUID uid);
+    
+}
