8a5c29f407b1c8e843b615018ddcbe80a3d5ab0d
slavb18 <slavb18@gmail.com>
2017-01-29 16:43:34 +0400

    archetype

--- archetype/archetype.sh
+++ archetype/archetype.sh
@@ -1,13 +1,8 @@
 #script to update jparestresource-archetype module from project
 set -e
 cd ..
-rm -rf jparestresource-archetype
+rm -rf frontend/node_modules/* frontend/node_modules/.bin
 mvn clean archetype:create-from-project -Darchetype.properties=archetype/archetype.properties
-mv target/generated-sources/archetype jparestresource-archetype
-grep -Rl jparestresource jparestresource-archetype/src/main/resources/archetype-resources|xargs sed -i 's/jparestresource/${rootArtifactId}/g'
-#mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/jparestresource jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__
-#mv jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/document jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/resources/ru/ilb/__rootArtifactId__/__object_urn__
-sed -i 's/documents.xjb/${domain_packageName}.xjb/' jparestresource-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/pom.xml
-patch -p0 < archetype/pom.xml.diff
-cd jparestresource-archetype
-mvn clean install
+#patch -p0 < archetype/pom.xml.diff
+cd target/generated-sources/archetype
+mvn clean install deploy
