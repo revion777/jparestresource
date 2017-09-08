89f50cb976918e795009b26248b1f5030f54c7e7
slavb18 <slavb18@gmail.com>
2017-09-08 11:28:17 +0400

    arhetype

--- archetype/archetype_test.sh
+++ archetype/archetype_test.sh
@@ -1,2 +1,3 @@
-rm -rf sampleapplication
-mvn -DarchetypeGroupId=ru.ilb.jparestresource -DarchetypeArtifactId=jparestresource-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=ru.ilb.sampleapplication -DartifactId=sampleapplication -Dversion=1.0-SNAPSHOT -Dpackage=ru.ilb.sampleapplication -Darchetype.interactive=false -DarchetypeCatalog=local --batch-mode --update-snapshots archetype:generate
+application=${1:-sampleapplication}
+rm -rf ${application}
+mvn -DarchetypeGroupId=ru.ilb.jparestresource -DarchetypeArtifactId=jparestresource-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=ru.ilb.${application} -DartifactId=${application} -Dversion=1.0-SNAPSHOT -Dpackage=ru.ilb.${application} -Darchetype.interactive=false -DarchetypeCatalog=local --batch-mode --update-snapshots archetype:generate
