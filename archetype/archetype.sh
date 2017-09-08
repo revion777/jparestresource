d37b16db29a2aa9acb48424685fd2ad969f25db1
slavb18 <slavb18@gmail.com>
2017-09-08 09:25:58 +0400

    fix archetype

--- archetype/archetype.sh
+++ archetype/archetype.sh
@@ -2,6 +2,7 @@
 set -e
 cd ../jparestresource
 mvn clean archetype:create-from-project -Darchetype.properties=../archetype/archetype.properties
+grep -Rl jparestresource target/generated-sources/archetype/src/main/resources/archetype-resources|xargs sed -i 's/jparestresource/${rootArtifactId}/g'
 #patch -p0 < archetype/pom.xml.diff
 cd target/generated-sources/archetype
 mvn clean install deploy
